2740e075cd9429e368d3bb200d6f56b6ca5fe000
Bemi <gbemigaadeosun@gmail.com>
2017-07-26 13:08:31 +0100

    SendData backend update with tonis

--- src/main/java/com/example/demo/controllers/RestFoodRecognitionController.java
+++ src/main/java/com/example/demo/controllers/RestFoodRecognitionController.java
@@ -1,14 +1,21 @@
 package com.example.demo.controllers;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/foodRecognition")
@@ -16,142 +23,144 @@ public class RestFoodRecognitionController {
 
 
     @RequestMapping(value = "/sendData", method = RequestMethod.POST, produces = "application/json")
-    public String sendData(final HttpServletRequest pRequest) throws IOException {
-        pRequest.getContentType();
-        System.out.print(pRequest.getContentType());
-
-
+    public ResponseEntity<Map> sendData(@RequestBody byte[] pRequest) throws IOException {
         String urlTo = "https://api-2445582032290.production.gw.apicast.io/v1/foodrecognition?user_key=ca87e5d9c66edd528225645ece8826d4";
         String result = null;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders h = new HttpHeaders();
+        h.setContentType(MediaType.IMAGE_JPEG);
+        return restTemplate.exchange(urlTo, HttpMethod.POST, new HttpEntity<>(pRequest, h), Map.class);
         //setup params
-        Map<String, String> params = new HashMap<String, String>(2);
-        params.put("foo", "");
-        params.put("bar", "");
-
-        try {
-            result = multipartRequest(urlTo, params, "test1.jpg", "picture", "picture/jpg");
-            System.out.print(result + " Final");
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+//        Map<String, String> params = new HashMap<String, String>(2);
+//        params.put("foo", "");
+//        params.put("bar", "");
+//
+//        String fileLocation = ((StandardMultipartHttpServletRequest) pRequest).getMultiFileMap().get("picture").get(0).getInputStream().;
+//
+//        try {
+//            result = multipartRequest(urlTo, params, "test1.jpg", "picture", "picture/jpg");
+//            System.out.print(result + " Final");
+//            return result;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 
-    public String multipartRequest(String urlTo, Map<String, String> parmas, String filepath, String filefield, String fileMimeType) throws Exception {
-        HttpURLConnection connection = null;
-        DataOutputStream outputStream = null;
-        InputStream inputStream = null;
+//    public String multipartRequest(String urlTo, Map<String, String> parmas, String filepath, String filefield, String fileMimeType) throws Exception {
+//        HttpURLConnection connection = null;
+//        DataOutputStream outputStream = null;
+//        InputStream inputStream = null;
+//
+//        String twoHyphens = "--";
+//        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
+//        String lineEnd = "\r\n";
+//
+//        String result = "";
+//
+//        int bytesRead, bytesAvailable, bufferSize;
+//        byte[] buffer;
+//        int maxBufferSize = 1 * 1024 * 1024;
+//        String[] q = filepath.split("/");
+//        int idx = q.length - 1;
+//
+//        try {
+//            File file = new File(filepath);
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//
+//            URL url = new URL(urlTo);
+//            connection = (HttpURLConnection) url.openConnection();
+//
+//            connection.setDoInput(true);
+//            connection.setDoOutput(true);
+//            connection.setUseCaches(false);
+//
+//            connection.setRequestMethod("POST");
+//            connection.setRequestProperty("Connection", "Keep-Alive");
+//            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+//            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+//
+//            outputStream = new DataOutputStream(connection.getOutputStream());
+//            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] + "\"" + lineEnd);
+//            outputStream.writeBytes("Content-Type: " + fileMimeType + lineEnd);
+//            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+//
+//            outputStream.writeBytes(lineEnd);
+//
+//            bytesAvailable = fileInputStream.available();
+//            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//            buffer = new byte[bufferSize];
+//
+//            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//            while (bytesRead > 0) {
+//                outputStream.write(buffer, 0, bufferSize);
+//                bytesAvailable = fileInputStream.available();
+//                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+//                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+//            }
+//
+//            outputStream.writeBytes(lineEnd);
+//
+//            // Upload POST Data
+//            Iterator<String> keys = parmas.keySet().iterator();
+//            while (keys.hasNext()) {
+//                String key = keys.next();
+//                String value = parmas.get(key);
+//
+//                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+//                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + lineEnd);
+//                outputStream.writeBytes("Content-Type: text/plain" + lineEnd);
+//                outputStream.writeBytes(lineEnd);
+//                outputStream.writeBytes(value);
+//                outputStream.writeBytes(lineEnd);
+//            }
+//
+//            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+//
+//
+//            if (200 != connection.getResponseCode()) {
+//                throw new Exception("Failed to upload code:" + connection.getResponseCode() + " " + connection.getResponseMessage());
+//            }
+//
+//            inputStream = connection.getInputStream();
+//
+//            result = this.convertStreamToString(inputStream);
+//
+//            fileInputStream.close();
+//            inputStream.close();
+//            outputStream.flush();
+//            outputStream.close();
+//
+//            return result;
+//        } catch (Exception e) {
+////            logger.error(e);
+//            throw new Exception(e);
+//        }
+//
+//    }
 
-        String twoHyphens = "--";
-        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
-        String lineEnd = "\r\n";
-
-        String result = "";
-
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-
-        String[] q = filepath.split("/");
-        int idx = q.length - 1;
-
-        try {
-            File file = new File(filepath);
-            FileInputStream fileInputStream = new FileInputStream(file);
-
-            URL url = new URL(urlTo);
-            connection = (HttpURLConnection) url.openConnection();
-
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setUseCaches(false);
-
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
-            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-
-            outputStream = new DataOutputStream(connection.getOutputStream());
-            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] + "\"" + lineEnd);
-            outputStream.writeBytes("Content-Type: " + fileMimeType + lineEnd);
-            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
-
-            outputStream.writeBytes(lineEnd);
-
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0) {
-                outputStream.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            }
-
-            outputStream.writeBytes(lineEnd);
-
-            // Upload POST Data
-            Iterator<String> keys = parmas.keySet().iterator();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                String value = parmas.get(key);
-
-                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + lineEnd);
-                outputStream.writeBytes("Content-Type: text/plain" + lineEnd);
-                outputStream.writeBytes(lineEnd);
-                outputStream.writeBytes(value);
-                outputStream.writeBytes(lineEnd);
-            }
-
-            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-
-            if (200 != connection.getResponseCode()) {
-                throw new Exception("Failed to upload code:" + connection.getResponseCode() + " " + connection.getResponseMessage());
-            }
-
-            inputStream = connection.getInputStream();
-
-            result = this.convertStreamToString(inputStream);
-
-            fileInputStream.close();
-            inputStream.close();
-            outputStream.flush();
-            outputStream.close();
-
-            return result;
-        } catch (Exception e) {
-//            logger.error(e);
-            throw new Exception(e);
-        }
-
-    }
-
-    private String convertStreamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
+//    private String convertStreamToString(InputStream is) {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        StringBuilder sb = new StringBuilder();
+//
+//        String line;
+//        try {
+//            while ((line = reader.readLine()) != null) {
+//                sb.append(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                is.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return sb.toString();
+//    }
 
 }
